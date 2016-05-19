6f683eb32866140e00d6465f24cfb384cfcef864
Steven Bakhtiari <steven.bakh@gmail.com>
2016-05-19 13:39:30 +0100

    Further improvements to run multiple eth nodes
    * Generate DAG once when building the image
    * Generate the account when actually running the node. This needs
      a rethink - only do it when the account hasn't been generated before.

---
+++ ethereum-node/run-node.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+geth --datadir ./chain --password ./account-password.txt account new > ./account.txt
+
+geth --datadir ./chain --networkid 4827 --password ./account-password.txt \
+     --verbosity 5 --rpc --rpccorsdomain "*" --nat "none" \
+     --rpcapi "admin,eth,debug,miner,net,shh,txpool,personal,web3" \
+     --mine --minerthreads 1 --identity ${NODE_NAME} --nodiscover
