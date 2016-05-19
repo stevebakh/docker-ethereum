e43c12c35d3b9ec9013fa5a03557a900b3412703
Steven Bakhtiari <steven.bakh@gmail.com>
2016-05-19 14:25:18 +0100

    Imrpove ability of SCM to see changes to bash

--- ethereum-node/run-node.sh
+++ ethereum-node/run-node.sh
@@ -2,7 +2,15 @@
 
 geth --datadir ./chain --password ./account-password.txt account new > ./account.txt
 
-geth --datadir ./chain --networkid 4827 --password ./account-password.txt \
-     --verbosity 5 --rpc --rpccorsdomain "*" --nat "none" \
+geth --datadir ./chain \
+     --networkid 4827 \
+     --password ./account-password.txt \
+     --verbosity 5 \
+     --rpc \
+     --rpccorsdomain "*" \
+     --nat "none" \
      --rpcapi "admin,eth,debug,miner,net,shh,txpool,personal,web3" \
-     --mine --minerthreads 1 --identity ${NODE_NAME} --nodiscover
+     --mine \
+     --minerthreads 1 \
+     --identity ${NODE_NAME} \
+     --nodiscover
