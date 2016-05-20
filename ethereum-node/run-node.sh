072050544a2a2b291806c0f4f2d99963eef71c69
Steven Bakhtiari <steven.bakh@gmail.com>
2016-05-20 09:26:22 +0100

    Automatically connect nodes to one another.
    When starting containers using docker-compose up and then docker-compose
    scale node=x, each node will attempt to connect to one another. Right
    now it seems to take several minutes for all to connect, but I'm not
    sure why. It works, though!

--- ethereum-node/run-node.sh
+++ ethereum-node/run-node.sh
@@ -1,18 +1,43 @@
 #!/bin/bash
 
-if [ ! -f ./account.txt ]; then
-    echo "First time running node, creating new account. See: ${PWD}/account.txt"
+# Get this node's 'enode url'. Other nodes will use this to connect.
+JS="'enode://' + admin.nodeInfo.id + '@' + '$(hostname -i):' + admin.nodeInfo.ports.discovery"
+ENODE_URL=$(geth --datadir ./chain --exec "${JS}" console 2>/dev/null)
+
+echo "Adding this node's  connection details to /tmp/nodes"
+echo $ENODE_URL >> /tmp/nodes
+
+# The following block will run every time a node is run for the first time. If
+# docker-compose down is executed, then all files are removed and this will run
+# again, as intended. If the containers are simply stopped or restarted using
+# docker, then the following block will not run.
+if [ ! -f ~/.intialised ]; then
+    echo "First time running node..." 
+    
+    echo "Creating new account. See: ${PWD}/account.txt"
     geth --datadir ./chain --password ./account-password.txt account new > ./account.txt
+    
+    touch ~/.initialised
 fi
+
+# Load any node connection details, filtering out the details for this node.
+while read -r LINE; do
+    if [ ${LINE} != ${ENODE_URL} ]; then
+        URL=$(echo ${LINE} | sed -e 's/^"\(.*\)"$/\1/')
+        BOOTNODES="${URL},${BOOTNODES}"
+    fi
+done < /tmp/nodes
+
+echo "Starting geth with bootnodes: ${BOOTNODES%?}"
 
 geth --datadir ./chain \
      --networkid 4827 \
      --password ./account-password.txt \
      --verbosity 5 \
+     --bootnodes "${BOOTNODES%?}" \
      --rpc \
      --rpccorsdomain "*" \
      --nat "none" \
      --rpcapi "admin,eth,debug,miner,net,shh,txpool,personal,web3" \
      --mine \
-     --minerthreads 1 \
-     --nodiscover
+     --minerthreads 1
